c262f546a88fb18322993f7a495d4a2abf66626a
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2024-06-27 13:19:14 +0530

    add: env dbName

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 const connectMongoDB = async () => {
 	const mongoURI = process.env.MONGO_URI;
+	const dbName = process.env.DB_NAME;
 	try {
-		const instance = await mongoose.connect(mongoURI.toString());
+		const instance = await mongoose.connect(`${mongoURI}/${dbName}`);
 		console.log(`MongoDB Connected: ${instance.connection.host}`);
 	} catch (error) {
 		throw Error(error.message);
